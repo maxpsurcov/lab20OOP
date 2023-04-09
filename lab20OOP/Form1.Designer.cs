8948941f9de1bc589b4231357a82313b7f7db121
maxpsurcov <max.psurcov1@gmail.com>
2023-04-09 18:05:18 +0300

    Добавьте файлы проекта.

---
+++ lab20OOP/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace lab20OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckLinesButton = new System.Windows.Forms.Button();
+            this.Line1ABox = new System.Windows.Forms.TextBox();
+            this.Line1BBox = new System.Windows.Forms.TextBox();
+            this.Line1CBox = new System.Windows.Forms.TextBox();
+            this.Line2ABox = new System.Windows.Forms.TextBox();
+            this.Line2BBox = new System.Windows.Forms.TextBox();
+            this.Line2CBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CheckLinesButton
+            // 
+            this.CheckLinesButton.Location = new System.Drawing.Point(89, 61);
+            this.CheckLinesButton.Name = "CheckLinesButton";
+            this.CheckLinesButton.Size = new System.Drawing.Size(264, 47);
+            this.CheckLinesButton.TabIndex = 0;
+            this.CheckLinesButton.Text = "CheckLinesButton";
+            this.CheckLinesButton.UseVisualStyleBackColor = true;
+            this.CheckLinesButton.Click += new System.EventHandler(this.CheckLinesButton_Click);
+            // 
+            // Line1ABox
+            // 
+            this.Line1ABox.Location = new System.Drawing.Point(89, 147);
+            this.Line1ABox.Name = "Line1ABox";
+            this.Line1ABox.Size = new System.Drawing.Size(137, 29);
+            this.Line1ABox.TabIndex = 1;
+            this.Line1ABox.Text = "A";
+            // 
+            // Line1BBox
+            // 
+            this.Line1BBox.Location = new System.Drawing.Point(89, 199);
+            this.Line1BBox.Name = "Line1BBox";
+            this.Line1BBox.Size = new System.Drawing.Size(137, 29);
+            this.Line1BBox.TabIndex = 2;
+            this.Line1BBox.Text = "B";
+            // 
+            // Line1CBox
+            // 
+            this.Line1CBox.Location = new System.Drawing.Point(89, 249);
+            this.Line1CBox.Name = "Line1CBox";
+            this.Line1CBox.Size = new System.Drawing.Size(137, 29);
+            this.Line1CBox.TabIndex = 3;
+            this.Line1CBox.Text = "C";
+            // 
+            // Line2ABox
+            // 
+            this.Line2ABox.Location = new System.Drawing.Point(311, 147);
+            this.Line2ABox.Name = "Line2ABox";
+            this.Line2ABox.Size = new System.Drawing.Size(137, 29);
+            this.Line2ABox.TabIndex = 4;
+            this.Line2ABox.Text = "A2";
+            // 
+            // Line2BBox
+            // 
+            this.Line2BBox.Location = new System.Drawing.Point(311, 199);
+            this.Line2BBox.Name = "Line2BBox";
+            this.Line2BBox.Size = new System.Drawing.Size(137, 29);
+            this.Line2BBox.TabIndex = 5;
+            this.Line2BBox.Text = "B2";
+            // 
+            // Line2CBox
+            // 
+            this.Line2CBox.Location = new System.Drawing.Point(311, 249);
+            this.Line2CBox.Name = "Line2CBox";
+            this.Line2CBox.Size = new System.Drawing.Size(137, 29);
+            this.Line2CBox.TabIndex = 6;
+            this.Line2CBox.Text = "C2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 618);
+            this.Controls.Add(this.Line2CBox);
+            this.Controls.Add(this.Line2BBox);
+            this.Controls.Add(this.Line2ABox);
+            this.Controls.Add(this.Line1CBox);
+            this.Controls.Add(this.Line1BBox);
+            this.Controls.Add(this.Line1ABox);
+            this.Controls.Add(this.CheckLinesButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckLinesButton;
+        private System.Windows.Forms.TextBox Line1ABox;
+        private System.Windows.Forms.TextBox Line1BBox;
+        private System.Windows.Forms.TextBox Line1CBox;
+        private System.Windows.Forms.TextBox Line2ABox;
+        private System.Windows.Forms.TextBox Line2BBox;
+        private System.Windows.Forms.TextBox Line2CBox;
+    }
+}
+
---
+++ lab20OOP/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab20OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void CheckLinesButton_Click(object sender, EventArgs e)
+        {
+            double A1 = Convert.ToDouble(Line1ABox.Text);
+            double B1 = Convert.ToDouble(Line1BBox.Text);
+            double C1 = Convert.ToDouble(Line1CBox.Text);
+            double A2 = Convert.ToDouble(Line2ABox.Text);
+            double B2 = Convert.ToDouble(Line2BBox.Text);
+            double C2 = Convert.ToDouble(Line2CBox.Text);
+
+            if (CheckIfLinesExist(A1, B1, C1, A2, B2, C2))
+            {
+                MessageBox.Show("Прямі існують");
+            }
+            else
+            {
+                MessageBox.Show("Прямі не існують");
+            }
+        }
+        // порожній клас-виключення
+        public class LinesException : Exception { }
+
+        // незалежний клас-виключення з полями-параметрами функції
+        public class LinesParametersException : Exception
+        {
+            public double A1 { get; }
+            public double B1 { get; }
+            public double C1 { get; }
+            public double A2 { get; }
+            public double B2 { get; }
+            public double C2 { get; }
+
+            public LinesParametersException(double a1, double b1, double c1, double a2, double b2, double c2)
+            {
+                A1 = a1;
+                B1 = b1;
+                C1 = c1;
+                A2 = a2;
+                B2 = b2;
+                C2 = c2;
+            }
+        }
+
+        // спадкоємець від стандартного виключення з полями
+        public class LinesCustomException : Exception
+        {
+            public double A { get; set; }
+            public double B { get; set; }
+            public double C { get; set; }
+
+            public LinesCustomException(string message, double a, double b, double c) : base(message)
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        }
+
+        public bool CheckIfLinesExist(double A1, double B1, double C1, double A2, double B2, double C2)
+        {
+            try
+            {
+                if (A1 == 0 && B1 == 0 && C1 == 0 && A2 == 0 && B2 == 0 && C2 == 0)
+                {
+                    throw new LinesException(); // порожній клас-виключення
+                }
+                if (A1 == 0 && B1 == 0 && C1 == 0)
+                {
+                    throw new LinesParametersException(A1, B1, C1, A2, B2, C2); // незалежний клас-виключення з полями-параметрами функції
+                }
+                double d = A1 * B2 - A2 * B1;
+                if (d == 0)
+                {
+                    throw new LinesCustomException("Прямі паралельні або збігаються", A1, B1, C1); // спадкоємець від стандартного виключення з полями
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (LinesException ex)
+            {
+                MessageBox.Show("Клас-виключення LinesException: порожній клас-виключення");
+                return false;
+            }
+            catch (LinesParametersException ex)
+            {
+                MessageBox.Show($"Клас-виключення LinesParametersException: A1={ex.A1}, B1={ex.B1} , C1={ex.C1}, A2={ex.A2}, B2={ex.B2}, C2={ex.C2}");
+                return false;
+            }
+            catch (LinesCustomException ex)
+            {
+                MessageBox.Show($"Клас-виключення LinesCustomException: {ex.Message}, A={ex.A}, B={ex.B}, C={ex.C}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Стандартний клас-виключення: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
+
